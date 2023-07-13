8843c15c1cb92c59f445de12191a5603b92796a2
georgeyiasemis <georgeyiasemis@hotmail.com>
2023-07-13 13:19:48 +0200

    Add mixed lpd

--- direct/nn/lpd/lpd_engine.py
+++ direct/nn/lpd/lpd_engine.py
@@ -5,11 +5,15 @@ from typing import Any, Callable, Dict, Optional, Tuple
 
 import torch
 from torch import nn
+from torch.cuda.amp import autocast
 
 import direct.data.transforms as T
 from direct.config import BaseConfig
-from direct.nn.mri_models import MRIModelEngine
 from direct.nn.ssl.mri_models import *
+from direct.engine import DoIterationOutput
+from direct.nn.mri_models import MRIModelEngine
+from direct.nn.ssl.mri_models import SSDUMRIModelEngine
+from direct.utils import detach_dict, dict_to_device
 
 
 class LPDNetEngine(MRIModelEngine):
@@ -183,3 +187,136 @@ class LPDNetN2NEngine(N2NMRIModelEngine):
             sampling_mask=data["noisier_sampling_mask"] if self.model.training else data["sampling_mask"],
         )
         return output_image, None
+
+
+class LPDNetMixedEngine(MRIModelEngine):
+    def __init__(
+        self,
+        cfg: BaseConfig,
+        model: nn.Module,
+        device: str,
+        forward_operator: Optional[Callable] = None,
+        backward_operator: Optional[Callable] = None,
+        mixed_precision: bool = False,
+        **models: nn.Module,
+    ):
+        """Inits :class:`LPDNetMixedEngine`.
+
+        Parameters
+        ----------
+        cfg: BaseConfig
+            Configuration file.
+        model: nn.Module
+            Model.
+        device: str
+            Device. Can be "cuda:{idx}" or "cpu".
+        forward_operator: Callable, optional
+            The forward operator. Default: None.
+        backward_operator: Callable, optional
+            The backward operator. Default: None.
+        mixed_precision: bool
+            Use mixed precision. Default: False.
+        **models: nn.Module
+            Additional models.
+        """
+        super().__init__(
+            cfg,
+            model,
+            device,
+            forward_operator=forward_operator,
+            backward_operator=backward_operator,
+            mixed_precision=mixed_precision,
+            **models,
+        )
+
+    def forward_function(self, data: Dict[str, Any]) -> None:
+        pass
+
+    def _do_iteration(
+        self,
+        data: Dict[str, Any],
+        loss_fns: Optional[Dict[str, Callable]] = None,
+        regularizer_fns: Optional[Dict[str, Callable]] = None,
+    ) -> DoIterationOutput:
+        """Performs forward method and calculates loss functions.
+
+        Parameters
+        ----------
+        data : Dict[str, Any]
+            Data containing keys with values tensors such as k-space, image, sensitivity map, etc.
+        loss_fns : Optional[Dict[str, Callable]]
+            Callable loss functions.
+        regularizer_fns : Optional[Dict[str, Callable]]
+            Callable regularization functions.
+
+        Returns
+        -------
+        DoIterationOutput
+            Contains outputs.
+        """
+
+        # loss_fns can be None, e.g. during validation
+        if loss_fns is None:
+            loss_fns = {}
+
+        loss_dict = {k: torch.tensor([0.0], dtype=data["target"].dtype).to(self.device) for k in loss_fns.keys()}
+
+        data = dict_to_device(data, self.device)
+
+        with autocast(enabled=self.mixed_precision):
+            data["sensitivity_map"] = self.compute_sensitivity_map(data["sensitivity_map"])
+
+            is_ssl_training = data["is_ssl_training"][0]
+
+            if is_ssl_training and self.model.training:
+                kspace, mask = data["input_kspace"], data["input_sampling_mask"]
+            else:
+                kspace, mask = data["masked_kspace"], data["sampling_mask"]
+
+            output_image = self.model(
+                masked_kspace=kspace,
+                sampling_mask=mask,
+                sensitivity_map=data["sensitivity_map"],
+            )
+
+            if self.model.training:
+                # Data consistency
+                output_kspace = T.apply_padding(
+                    kspace + self._forward_operator(output_image, data["sensitivity_map"], ~mask),
+                    padding=data["padding"],
+                )
+                if is_ssl_training:
+                    # Project predicted k-space onto target k-space if SSL
+                    output_kspace = T.apply_mask(output_kspace, data["target_sampling_mask"], return_mask=False)
+
+                # Compute k-space loss
+                loss_dict = self.compute_loss_on_data(loss_dict, loss_fns, data, None, output_kspace)
+
+                # SENSE reconstruction if SSL else modulus if supervised
+                output_images = T.modulus(
+                    T.reduce_operator(
+                        self.backward_operator(output_kspace, dim=self._spatial_dims),
+                        data["sensitivity_map"],
+                        self._coil_dim,
+                    )
+                    if is_ssl_training
+                    else output_image
+                )
+
+                # Compute image loss
+                loss_dict = self.compute_loss_on_data(loss_dict, loss_fns, data, output_images, None)
+
+                loss = sum(loss_dict.values())  # type: ignore
+
+                self._scaler.scale(loss).backward()
+
+            else:
+                output_image = T.modulus(output_image)
+
+        loss_dict = detach_dict(loss_dict)  # Detach dict, only used for logging.
+
+        return DoIterationOutput(
+            output_image=output_image,
+            sensitivity_map=data["sensitivity_map"],
+            data_dict={**loss_dict},
+        )
