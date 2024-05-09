1a0a211012fdf9300a6aa356c3a77d5a0b96f20d
georgeyiasemis <georgeyiasemis@hotmail.com>
2024-05-09 19:01:55 +0200

    Old engine

--- direct/nn/vsharp/vsharp_engine.py
+++ direct/nn/vsharp/vsharp_engine.py
@@ -26,8 +26,8 @@ class VSharpNet3DEngine(MRIModelEngine):
         cfg: BaseConfig,
         model: nn.Module,
         device: str,
-        forward_operator: Optional[Callable[[tuple[Any, ...]], torch.Tensor]] = None,
-        backward_operator: Optional[Callable[[tuple[Any, ...]], torch.Tensor]] = None,
+        forward_operator: Optional[Callable] = None,
+        backward_operator: Optional[Callable] = None,
         mixed_precision: bool = False,
         **models: nn.Module,
     ):
@@ -41,9 +41,9 @@ class VSharpNet3DEngine(MRIModelEngine):
             Model.
         device: str
             Device. Can be "cuda:{idx}" or "cpu".
-        forward_operator: Callable[[tuple[Any, ...]], torch.Tensor], optional
+        forward_operator: Callable, optional
             The forward operator. Default: None.
-        backward_operator: Callable[[tuple[Any, ...]], torch.Tensor], optional
+        backward_operator: Callable, optional
             The backward operator. Default: None.
         mixed_precision: bool
             Use mixed precision. Default: False.
@@ -64,20 +64,20 @@ class VSharpNet3DEngine(MRIModelEngine):
 
     def _do_iteration(
         self,
-        data: dict[str, Any],
-        loss_fns: Optional[dict[str, callable]] = None,
-        regularizer_fns: Optional[dict[str, callable]] = None,
+        data: Dict[str, Any],
+        loss_fns: Optional[Dict[str, Callable]] = None,
+        regularizer_fns: Optional[Dict[str, Callable]] = None,
     ) -> DoIterationOutput:
         """Performs forward method and calculates loss functions.
 
         Parameters
         ----------
-        data : dict[str, Any]
+        data : Dict[str, Any]
             Data containing keys with values tensors such as k-space, image, sensitivity map, etc.
-        loss_fns : Optional[dict[str, callable]]
-            callable loss functions.
-        regularizer_fns : Optional[dict[str, callable]]
-            callable regularization functions.
+        loss_fns : Optional[Dict[str, Callable]]
+            Callable loss functions.
+        regularizer_fns : Optional[Dict[str, Callable]]
+            Callable regularization functions.
 
         Returns
         -------
@@ -95,75 +95,48 @@ class VSharpNet3DEngine(MRIModelEngine):
         output_kspace: TensorOrNone
 
         with autocast(enabled=self.mixed_precision):
-            output_images, _ = self.forward_function(data)
+            output_images, output_kspace = self.forward_function(data)
+            # output_images = [
+            #     T.modulus_if_complex(_, complex_axis=self._complex_dim)
+            #     for _ in output_images
+            # ]
             loss_dict = {k: torch.tensor([0.0], dtype=data["target"].dtype).to(self.device) for k in loss_fns.keys()}
 
-            if self.model.training:
-                auxiliary_loss_weights = torch.logspace(-1, 0, steps=len(output_images)).to(output_images[0])
-                for i, output_image in enumerate(output_images):
-
-                    output_kspace = data["masked_kspace"] + T.apply_mask(
-                        T.apply_padding(
-                            self.forward_operator(
-                                T.expand_operator(output_image, data["sensitivity_map"], dim=self._coil_dim),
-                                dim=self._spatial_dims,
-                            ),
-                            padding=data.get("padding", None),
-                        ),
-                        ~data["sampling_mask"],
-                        return_mask=False,
-                    )
-                    # Compute loss on k-space
-                    loss_dict = self.compute_loss_on_data(
-                        loss_dict, loss_fns, data, None, output_kspace, auxiliary_loss_weights[i]
-                    )
-
-                    output_image = T.modulus(
-                        T.reduce_operator(
-                            self.backward_operator(output_kspace, dim=self._spatial_dims),
-                            data["sensitivity_map"],
-                            self._coil_dim,
-                        )
-                    )
-
-                    # Compute loss on image domain
-                    loss_dict = self.compute_loss_on_data(
-                        loss_dict, loss_fns, data, output_image, None, auxiliary_loss_weights[i]
-                    )
-
-                loss = sum(loss_dict.values())  # type: ignore
-
-                self._scaler.scale(loss).backward()
-            else:
-                output_kspace = data["masked_kspace"] + T.apply_mask(
-                    T.apply_padding(
-                        self.forward_operator(
-                            T.expand_operator(output_images[-1], data["sensitivity_map"], dim=self._coil_dim),
-                            dim=self._spatial_dims,
-                        ),
-                        padding=data.get("padding", None),
-                    ),
-                    ~data["sampling_mask"],
-                    return_mask=False,
+            auxiliary_loss_weights = torch.logspace(-1, 0, steps=len(output_images)).to(output_images[0])
+            for i in range(len(output_images)):
+                loss_dict = self.compute_loss_on_data(
+                    loss_dict,
+                    loss_fns,
+                    data,
+                    T.modulus(output_images[i]),
+                    None,
+                    auxiliary_loss_weights[i],
                 )
 
-                output_image = T.modulus(
-                    T.reduce_operator(
-                        self.backward_operator(output_kspace, dim=self._spatial_dims),
-                        data["sensitivity_map"],
-                        self._coil_dim,
-                    )
-                )
+            loss_dict = self.compute_loss_on_data(
+                loss_dict,
+                loss_fns,
+                data,
+                None,
+                output_kspace,
+                auxiliary_loss_weights[i],
+            )
 
-            loss_dict = detach_dict(loss_dict)  # Detach dict, only used for logging.
+            loss = sum(loss_dict.values())  # type: ignore
 
+        if self.model.training:
+            self._scaler.scale(loss).backward()
+
+        loss_dict = detach_dict(loss_dict)  # Detach dict, only used for logging.
+
+        output_image = output_images[-1]
         return DoIterationOutput(
             output_image=output_image,
             sensitivity_map=data["sensitivity_map"],
             data_dict={**loss_dict},
         )
 
-    def forward_function(self, data: dict[str, Any]) -> tuple[torch.Tensor, None]:
+    def forward_function(self, data: Dict[str, Any]) -> Tuple[torch.Tensor, None]:
         data["sensitivity_map"] = self.compute_sensitivity_map(data["sensitivity_map"])
 
         output_images = self.model(
@@ -173,7 +146,17 @@ class VSharpNet3DEngine(MRIModelEngine):
         )  # shape (batch, height,  width, complex[=2])
 
         output_image = output_images[-1]
-        output_kspace = None
+        output_kspace = data["masked_kspace"] + T.apply_mask(
+            T.apply_padding(
+                self.forward_operator(
+                    T.expand_operator(output_image, data["sensitivity_map"], dim=self._coil_dim),
+                    dim=self._spatial_dims,
+                ),
+                padding=data.get("padding", None),
+            ),
+            ~data["sampling_mask"],
+            return_mask=False,
+        )
 
         return output_images, output_kspace
 
